176b1c79869e25a2b924f68569bf799329a7f98b
ARostovsky <fixer.spb@gmail.com>
2017-03-10 17:36:37 +0300

    refactoring according to third review [2 part]: Installer class is added

--- src/main/groovy/patchtest.groovy
+++ src/main/groovy/patchtest.groovy
@@ -51,37 +51,13 @@ timeout = map.timeout.toInteger()
 tempDirectory = Files.createTempDirectory('patchtest_')
 
 
-/**
- * This is a Build class
- * @param binding               Global variables.
- * @param buildFolder           This is a path to folder, where build is placed. There is a difference with
- *                              installation folder: buildFolder is a directory where 'bin', 'lib' and 'plugins' folders
- *                              are located and installation folder is a folder where build is installed/unpacked by
- *                              installer or archive manager. It can be like:
- *                              ...
- *                              └── prev <installation folder>
- *                                  └── pycharm-172.339 <buildFolder>
- *                                      ├── bin
- *                                      ├── lib
- *                                      ├── plugins
- *                                      └── ...
- *
- * @param edition               Two-letter code like "PC" (PyCharm Community Edition) or "PY" (PyCharm Professional
- *                              edition) should be specified here if any. If there is no editions - empty string
- *                              should be specified then.
- * @param installerName         Installer name, like "pycharmPY-171.3566.25.exe" or "pycharmPC-171.3566.25.tar.gz".
- * @param buildNumber           This is a build number, like "171.2342.5" or "171.3566.25".
- */
-class Build{
-    Binding binding
-    Path buildFolder
-    String edition
-    String installerName
+class Installer{
+    private Binding binding
     String buildNumber
-    File log4jJar
+    String installerName
 
     /**
-     * This is a Build constructor
+     * This is an Installer constructor
      * @param edition           Two-letter code like "PC" (PyCharm Community Edition) or "PY" (PyCharm Professional
      *                          edition) should be specified here if any. If there is no editions - empty string
      *                          should be specified then.
@@ -89,10 +65,9 @@ class Build{
      * @param binding           Global variables.
      * @param withBundledJdk    Is this build with included jdk or not, default is true.
      */
-    Build(String edition, String buildNumber, Binding binding, boolean withBundledJdk=true){
-        this.edition = edition
-        this.buildNumber = buildNumber
+    Installer(String edition, String buildNumber, Binding binding, boolean withBundledJdk=true){
         this.binding = binding
+        this.buildNumber = buildNumber
         this.installerName = sprintf('%s%s-%s%s.%s', [binding.product,
                                                       edition,
                                                       buildNumber,
@@ -102,6 +77,109 @@ class Build{
 
     File getInstallerPath(String installer=installerName){
         return new File(binding.tempDirectory.toString(), installer)
+    }
+
+    private void download(){
+        ArrayList<org.jetbrains.teamcity.rest.Build> builds = TeamCityInstance["Companion"]
+                .guestAuth("http://buildserver.labs.intellij.net")
+                .builds()
+                .fromConfiguration(new BuildConfigurationId(binding.buildConfigurationID))
+                .list()
+        builds.each { build ->
+            if (build.buildNumber == this.buildNumber){
+                println(build)
+                build.downloadArtifact(installerName, this.getInstallerPath().getAbsoluteFile())
+                return true
+            }
+        }
+    }
+
+    private Path install(Path installationFolder){
+        AntBuilder ant = new AntBuilder()
+        ant.mkdir(dir: installationFolder.toString())
+        File pathToInstaller = this.getInstallerPath()
+
+        switch (binding.os) {
+            case OS.WIN:
+                ant.exec(executable: "cmd", failonerror: "True") {
+                    arg(line: "/k $pathToInstaller /S /D=$installationFolder.absolutePath && ping 127.0.0.1 -n $binding.timeout > nul")
+                }
+                return installationFolder
+            case OS.LINUX:
+                ant.gunzip(src: pathToInstaller)
+                String tar = installerName[0..-1-".gz".length()]
+                ant.untar(src: this.getInstallerPath(tar), dest: installationFolder)
+
+                return getBuildFolder(installationFolder, 1)
+            case OS.MAC:
+                println("##teamcity[blockOpened name='unzip output']")
+                ant.exec(executable: "unzip", failonerror: "True"){
+                    arg(line: "$pathToInstaller -d $installationFolder")
+                }
+                println("##teamcity[blockClosed name='unzip output']")
+
+                return getBuildFolder(installationFolder, 2)
+            default:
+                throw new RuntimeException(sprintf("Wrong os: $binding.os"))
+        }
+    }
+
+    private Path getBuildFolder(Path folder, int depth=1){
+        assert depth > 0
+        assert Files.list(folder).count() == 1
+        Path buildFolder = null
+
+        if ( depth == 1 ){
+            folder.eachDir { directory ->
+                buildFolder = Paths.get(directory.toString())
+            }
+        } else if ( depth > 1 ){
+            folder.eachDir { directory ->
+                buildFolder = getBuildFolder(Paths.get(directory.toString()), depth - 1)
+            }
+        }
+
+        if (!buildFolder){
+            throw new RuntimeException("buildFolder's path wasn't found")
+        }
+
+        return buildFolder
+    }
+
+    Build installBuild(Path installationFolder){
+        download()
+        Path buildFolder = install(installationFolder)
+        return new Build(buildFolder, binding)
+    }
+
+}
+
+
+
+class Build{
+    private Binding binding
+    Path buildFolder
+    private File log4jJar
+
+    /**
+     * This is a Build class
+     * @param buildFolder           This is a path to folder, where build is placed. There is a difference with
+     *                              installation folder: buildFolder is a directory where 'bin', 'lib' and 'plugins' folders
+     *                              are located and installation folder is a folder where build is installed/unpacked by
+     *                              installer or archive manager. It can be like:
+     *                              ...
+     *                              └── prev <installation folder>
+     *                                  └── pycharm-172.339 <buildFolder>
+     *                                      ├── bin
+     *                                      ├── lib
+     *                                      ├── plugins
+     *                                      └── ...
+     * @param binding               Global variables.
+     */
+    Build(Path buildFolder, Binding binding){
+        this.buildFolder = buildFolder
+        this.binding = binding
+
     }
 
     String calcChecksum(){
@@ -122,71 +200,8 @@ class Build{
         return ant.project.properties.sum
     }
 
-    void downloadBuild(){
-        ArrayList<org.jetbrains.teamcity.rest.Build> builds = TeamCityInstance["Companion"]
-                .guestAuth("http://buildserver.labs.intellij.net")
-                .builds()
-                .fromConfiguration(new BuildConfigurationId(binding.buildConfigurationID))
-                .list()
-        builds.each { build ->
-            if (build.buildNumber == this.buildNumber){
-                println(build)
-                build.downloadArtifact(installerName, this.getInstallerPath().getAbsoluteFile())
-                return true
-            }
-        }
-    }
-
-    void install(Path installationFolder){
-        AntBuilder ant = new AntBuilder()
-        ant.mkdir(dir: installationFolder.toString())
-        File pathToInstaller = this.getInstallerPath()
-
-        switch (binding.os) {
-            case OS.WIN:
-                ant.exec(executable: "cmd", failonerror: "True") {
-                    arg(line: "/k $pathToInstaller /S /D=$installationFolder.absolutePath && ping 127.0.0.1 -n $binding.timeout > nul")
-                }
-                buildFolder = installationFolder
-                break
-            case OS.LINUX:
-                ant.gunzip(src: pathToInstaller)
-                String tar = installerName[0..-1-".gz".length()]
-                ant.untar(src: this.getInstallerPath(tar), dest: installationFolder)
-
-                defineBuildFolder(installationFolder, 1)
-                break
-            case OS.MAC:
-                println("##teamcity[blockOpened name='unzip output']")
-                ant.exec(executable: "unzip", failonerror: "True"){
-                    arg(line: "$pathToInstaller -d $installationFolder")
-                }
-                println("##teamcity[blockClosed name='unzip output']")
-
-                defineBuildFolder(installationFolder, 2)
-                break
-            default:
-                throw new RuntimeException(sprintf("Wrong os: $binding.os"))
-        }
-    }
-
-    void defineBuildFolder(Path folder, int depth=1){
-        assert depth > 0
-        assert Files.list(folder).count() == 1
-
-        if ( depth == 1 ){
-            folder.eachDir { directory ->
-                this.buildFolder = Paths.get(directory.toString())
-            }
-        } else if ( depth > 1 ){
-            folder.eachDir { directory ->
-                defineBuildFolder(Paths.get(directory.toString()), depth - 1)
-            }
-        }
-    }
-
     void patch(File patch){
-        this.buildFolder.toFile().eachFileRecurse(FileType.FILES) { file ->
+        buildFolder.toFile().eachFileRecurse(FileType.FILES) { file ->
             if (file.name.contains('log4j.jar')) {
                 this.log4jJar = file
             }
@@ -239,22 +254,19 @@ def main(String dir='patches'){
         println(sprintf("##teamcity[testStarted name='%s']", testName))
 
         try {
-            Build prev = new Build(partsOfPatchName.get(0), partsOfPatchName.get(1), binding, withBundledJdk)
-            Build curr = new Build(partsOfPatchName.get(0), partsOfPatchName.get(2), binding, withBundledJdk)
+            Installer prevInstaller = new Installer(partsOfPatchName.get(0), partsOfPatchName.get(1), binding, withBundledJdk)
+            Build prevBuild = prevInstaller.installBuild(Paths.get(tempDirectory.toString(), "prev"))
+            prevBuild.calcChecksum()
+            prevBuild.patch(patch)
+            String prevChecksum = prevBuild.calcChecksum()
 
-            prev.downloadBuild()
-            prev.install(Paths.get(tempDirectory.toString(), "prev"))
-            prev.calcChecksum()
-            prev.patch(patch)
-            String prev_checksum = prev.calcChecksum()
+            Installer currInstaller = new Installer(partsOfPatchName.get(0), partsOfPatchName.get(2), binding, withBundledJdk)
+            Build currBuild = currInstaller.installBuild(Paths.get(tempDirectory.toString(), "curr"))
+            String currChecksum = currBuild.calcChecksum()
 
-            curr.downloadBuild()
-            curr.install(Paths.get(tempDirectory.toString(), "curr"))
-            String curr_checksum = prev.calcChecksum()
-
-            if (prev_checksum != curr_checksum){
+            if (prevChecksum != currChecksum){
                 println(sprintf("##teamcity[testFailed name='%s'] message='Checksums are different: %s and %s']",
-                        [testName, prev_checksum, curr_checksum]))
+                        [testName, prevChecksum, currChecksum]))
             }
         }
         catch (e) {
