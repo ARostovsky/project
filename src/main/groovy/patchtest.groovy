02c583e4cfb8b3c43edfee3ed63bf396d9d88bb8
Aleksey.Rostovskiy <fixer.spb@gmail.com>
2017-04-24 16:02:23 +0300

    completely remove sprintf and binding

--- src/main/groovy/patchtest.groovy
+++ src/main/groovy/patchtest.groovy
@@ -15,63 +15,71 @@ enum OS {
         return name == "unix" ? LINUX : valueOf(name.toUpperCase())
     }
 
-    def extension(binding) {
+    String extension(String winExtension) {
         switch (this) {
             case WIN:
-                return binding.winExtension
+                return winExtension in ['zip', 'exe', 'win.zip'] ? winExtension : 'zip'
             case LINUX:
                 return "tar.gz"
             case MAC:
                 return "sit"
+            default:
+                throw new IllegalArgumentException("Wrong os: $this")
         }
     }
 }
 
-Map<String, String> map = evaluate(Arrays.toString(args)) as Map<String, String>
-println("Args: $map")
 
-/**
- * @value product               Product name, should be specified just like it is specified in installer:
- *                              "pycharm", "idea", "PhpStorm", etc
- *
- * @value os {@link OS}
- *
- * @value winExtension          Extension for Windows installers, can be "zip" (by default), "exe" or 'win.zip' (IDEA)
- *
- * @value extension             Extension value of installer, can be "exe"/"zip" (win.zip), "tar.gz" or "sit" according to OS
- *
- * @value buildConfigurationIDs List of TeamCity's buildConfigurationID, like "ijplatform_master_PyCharm",
- *                              "ijplatform_master_Idea", "ijplatform_master_PhpStorm", etc.
- *
- * @value timeout               Timeout in seconds, used for Windows installation (using .exe) and patching processes.
- *                              By default it's 60 seconds for Windows installation, but for patching it's multiplied
- *                              by 3. Can be passed as a parameter via TeamCity for reasons like slow installation or
- *                              patching in different IDEs.
- *
- * @value out                   Folder for artifacts that should be saved after test. Used for store patch logs.
- *
- */
-product = map.product
-os = OS.fromPatch(map.platform)
-winExtension =  map.winExtension in ['zip', 'exe', 'win.zip'] ? map.winExtension : 'zip'
-extension = os.extension(binding)
-buildConfigurationIDs = map.buildConfigurationID.split(';')
-timeout = map.timeout.toInteger()
-out = Paths.get(map.out)
-tempDirectory = Files.createTempDirectory('patchtest_')
+class Globals {
+    /**
+     * @value product                Product name, should be specified just like it is specified in installer:
+     *                               "pycharm", "idea", "PhpStorm", etc
+     * @value os {@link OS}
+     * @value extension              Extension value of installer, can be "exe"/"zip" (win.zip for IDEA), "tar.gz" or
+     *                               "sit" according to OS
+     * @value buildConfigurationIDs  List of TeamCity's buildConfigurationID, like "ijplatform_master_PyCharm",
+     *                               "ijplatform_master_Idea", "ijplatform_master_PhpStorm", etc.
+     * @value timeout                Timeout in seconds, used for Windows installation (using .exe) and patching
+     *                               processes. By default it's 60 seconds for Windows installation, but for patching
+     *                               it's multiplied by 3. Can be passed as a parameter via TeamCity for reasons like
+     *                               slow installation or patching in different IDEs.
+     * @value out                    Folder for artifacts that should be saved after test. Used for store patch logs.
+     */
+    String product
+    OS os
+    String extension
+    List<String> buildConfigurationIDs
+    Integer timeout
+    Path out
+    Path tempDirectory
+
+    Globals(Map<String, String> map) {
+        product = map.product
+        os = OS.fromPatch(map.platform)
+        println(os)
+        /**
+        * @value winExtension        Extension for Windows installers, can be "zip" (by default), "exe" or 'win.zip' (IDEA)
+        */
+        extension = os.extension(map.winExtension)
+        buildConfigurationIDs = map.buildConfigurationID.split(';')
+        timeout = map.timeout.toInteger()
+        out = Paths.get(map.out)
+        tempDirectory = Files.createTempDirectory('patchtest_')
+    }
+
+}
 
 
-class WrongConfigurationException extends RuntimeException
-{
+class WrongConfigurationException extends RuntimeException {
     WrongConfigurationException(String message)
     {
-        super(message);
+        super(message)
     }
 }
 
 
 class Installer {
-    private Binding binding
+    private Globals globals
     String buildNumber
     String installerName
 
@@ -81,26 +89,22 @@ class Installer {
      * @param edition           Two-letter code like "PC" (PyCharm Community Edition) or "PY" (PyCharm Professional
      *                          edition) should be specified here if product has more than 1 configuration.
      *                          If there is no editions - empty string should be specified then.
-     * @param binding           Global variables.
+     * @param globals           Global variables.
      * @param withBundledJdk    Is this build with included jdk or not, default is true.
      */
-    Installer(String buildNumber, String edition, Binding binding, boolean withBundledJdk = true) {
-        this.binding = binding
+    Installer(String buildNumber, String edition, Globals globals, boolean withBundledJdk = true) {
+        this.globals = globals
         this.buildNumber = buildNumber
-        this.installerName = sprintf('%s%s-%s%s.%s', [binding.product,
-                                                      edition,
-                                                      buildNumber,
-                                                      (withBundledJdk) ? '' : '-no-jdk',
-                                                      binding.extension])
+        this.installerName = "${globals.product}${edition}-${buildNumber}${(withBundledJdk) ? '' : '-no-jdk'}.${globals.extension}"
     }
 
     private File getInstallerPath(String installer = installerName) {
-        return new File(binding.tempDirectory.toString(), installer)
+        return new File(globals.tempDirectory.toString(), installer)
     }
 
     private void download() {
         BuildArtifact artifact = null
-        binding.buildConfigurationIDs.each { String buildConfigurationID ->
+        globals.buildConfigurationIDs.each { String buildConfigurationID ->
             ArrayList<org.jetbrains.teamcity.rest.Build> builds = TeamCityInstance["Companion"]
                     .guestAuth("http://buildserver.labs.intellij.net")
                     .builds()
@@ -120,7 +124,7 @@ class Installer {
             }
         }
         if (!artifact) {
-            throw new WrongConfigurationException("Didn't find build $buildNumber in configurations: $binding.buildConfigurationIDs")
+            throw new WrongConfigurationException("Didn't find build $buildNumber in configurations: $globals.buildConfigurationIDs")
         }
     }
 
@@ -149,16 +153,16 @@ class Installer {
         ant.mkdir(dir: installationFolder.toString())
         File pathToInstaller = getInstallerPath()
 
-        switch (binding.os) {
+        switch (globals.os) {
             case OS.WIN:
-                if (binding.extension == 'exe') {
+                if (globals.extension == 'exe') {
                     ant.exec(executable: "cmd", failonerror: "True") {
-                        arg(line: "/k $pathToInstaller /S /D=$installationFolder.absolutePath && ping 127.0.0.1 -n $binding.timeout > nul")
+                        arg(line: "/k $pathToInstaller /S /D=$installationFolder.absolutePath && ping 127.0.0.1 -n $globals.timeout > nul")
                     }
-                } else if (binding.extension.contains('zip')) {
+                } else if (globals.extension.contains('zip')) {
                     ant.unzip(src: pathToInstaller, dest: installationFolder)
                 } else {
-                    throw new IllegalArgumentException("Wrong extension: $binding.extension")
+                    throw new IllegalArgumentException("Wrong extension: $globals.extension")
                 }
                 return installationFolder
             case OS.LINUX:
@@ -176,7 +180,7 @@ class Installer {
 
                 return getBuildFolder(installationFolder, 2)
             default:
-                throw new IllegalArgumentException("Wrong os: $binding.os")
+                throw new IllegalArgumentException("Wrong os: $globals.os")
         }
     }
 
@@ -198,14 +202,14 @@ class Installer {
     Build installBuild(Path installationFolder) {
         download()
         Path buildFolder = install(installationFolder)
-        return new Build(buildFolder, binding)
+        return new Build(buildFolder, globals)
     }
 
 }
 
 
 class Build {
-    private Binding binding
+    private Globals globals
     Path buildFolder
 
     /**
@@ -221,15 +225,15 @@ class Build {
      *                              ├── lib
      *                              ├── plugins
      *                              └── ...
-     * @param binding       Global variables.
+     * @param globals       Global variables.
      */
-    Build(Path buildFolder, Binding binding) {
+    Build(Path buildFolder, Globals globals) {
         this.buildFolder = buildFolder
-        this.binding = binding
+        this.globals = globals
     }
 
     String calcChecksum() {
-        Path checksumFolder = binding.tempDirectory.resolve("checksums")
+        Path checksumFolder = globals.tempDirectory.resolve("checksums")
 
         AntBuilder ant = new AntBuilder()
         println('')
@@ -237,10 +241,10 @@ class Build {
         ant.mkdir(dir: checksumFolder)
         ant.checksum(todir: checksumFolder, totalproperty: 'sum') {
             fileset(dir: buildFolder) {
-                if (binding.os == OS.WIN) {
+                if (globals.os == OS.WIN) {
                     exclude(name: "**\\Uninstall.exe")
                     exclude(name: "**\\classes.jsa")
-                } else if (binding.os == OS.MAC){
+                } else if (globals.os == OS.MAC){
                     exclude(name: "**\\*.dylib")
                 }
             }
@@ -264,14 +268,14 @@ class Build {
         AntBuilder ant = new AntBuilder()
         println('')
         ant.echo("Applying patch $patch.name")
-        Path out = binding.out.resolve(patch.name)
+        Path out = globals.out.resolve(patch.name)
         ant.mkdir(dir: out)
 
         ant.java(classpath: ant.path {pathelement(path: patch); pathelement(path: log4jJar)},
                  classname: "com.intellij.updater.Runner",
                  fork: "true",
                  maxmemory: "800m",
-                 timeout: binding.timeout * 3000) {
+                 timeout: globals.timeout * 3000) {
             jvmarg(value: "-Didea.updater.log=$out")
             arg(line: "install '$buildFolder'")
         }
@@ -292,7 +296,10 @@ static ArrayList<File> findFiles(String extension, File directory = new File('.'
     return list
 }
 
-def main(String dir = 'patches') {
+
+def main(Map<String, String> map, String dir = 'patches') {
+    Globals globals = new Globals(map)
+
     ArrayList<File> patches = findFiles(mask = '.jar', directory = new File(dir))
     println("##teamcity[enteredTheMatrix]")
     println("##teamcity[testCount count='$patches.size']")
@@ -307,45 +314,41 @@ def main(String dir = 'patches') {
         edition = edition in ['IC', 'IU', 'PC', 'PY', 'PE'] ? edition : ''
         edition = (edition == 'PE') ? 'EDU' : edition
 
-        String product = binding.product.substring(0, 1).toUpperCase() + binding.product.substring(1)
-        String testName = sprintf("%s %s%s%s test, patch name: %s", [product,
-                                                                     edition,
-                                                                     (withBundledJdk) ? '' : ' (no-jdk)',
-                                                                     (edition || !withBundledJdk) ? ' edition' : '',
-                                                                     patchName])
-        println(sprintf("##teamcity[testStarted name='%s']", testName))
+        String product = globals.product.substring(0, 1).toUpperCase() + globals.product.substring(1)
+        String testName = "${product} ${edition}${withBundledJdk ? '' : ' (no-jdk)'}${(edition || !withBundledJdk) ? ' edition' : ''} test, patch name: $patchName"
+        println("##teamcity[testStarted name='$testName']")
 
         try {
-            new AntBuilder().mkdir(dir: tempDirectory.toString())
-            Installer prevInstaller = new Installer(partsOfPatchName.get(1), edition, binding, withBundledJdk)
-            Build prevBuild = prevInstaller.installBuild(tempDirectory
-                    .resolve(sprintf("previous-%s-%s",[partsOfPatchName.get(0), partsOfPatchName.get(1)])))
+            new AntBuilder().mkdir(dir: globals.tempDirectory.toString())
+            Installer prevInstaller = new Installer(partsOfPatchName.get(1), edition, globals, withBundledJdk)
+            Build prevBuild = prevInstaller.installBuild(globals.tempDirectory.resolve("previous-${partsOfPatchName.get(0)}-${partsOfPatchName.get(1)}"))
             prevBuild.calcChecksum()
             prevBuild.patch(patch)
             String prevChecksum = prevBuild.calcChecksum()
 
-            Installer currInstaller = new Installer(partsOfPatchName.get(2), edition, binding, withBundledJdk)
-            Build currBuild = currInstaller.installBuild(tempDirectory
-                    .resolve(sprintf("current-%s-%s", [partsOfPatchName.get(0), partsOfPatchName.get(2)])))
+            Installer currInstaller = new Installer(partsOfPatchName.get(2), edition, globals, withBundledJdk)
+            Build currBuild = currInstaller.installBuild(globals.tempDirectory.resolve("current-${partsOfPatchName.get(0)}-${partsOfPatchName.get(2)}"))
             String currChecksum = currBuild.calcChecksum()
 
             if (prevChecksum != currChecksum) {
-                println(sprintf("##teamcity[testFailed name='%s'] message='Checksums are different: %s and %s']",
-                        [testName, prevChecksum, currChecksum]))
+                println("##teamcity[testFailed name='$testName'] message='Checksums are different: $prevChecksum and $currChecksum']")
             }
         }
         catch (WrongConfigurationException e) {
-            println(sprintf("##teamcity[testIgnored name='%s'] message='Check your build configuration: %s']", [testName, e.message]))
+            println("##teamcity[testIgnored name='$testName'] message='Check your build configuration: $e.message']")
         }
         catch (e){
-            println(sprintf("##teamcity[testFailed name='%s'] message='%s']", [testName, e]))
+            println("##teamcity[testFailed name='$testName'] message='$e']")
             e.printStackTrace()
         } finally {
-            new AntBuilder().delete(dir: tempDirectory.toString())
-            println(sprintf("##teamcity[testFinished name='%s']", testName))
+            new AntBuilder().delete(dir: globals.tempDirectory.toString())
+            println("##teamcity[testFinished name='$testName']")
         }
     }
     println("##teamcity[testSuiteFinished name='Patch Update Autotest']")
 }
 
-main()
+
+Map<String, String> map = evaluate(Arrays.toString(args)) as Map<String, String>
+println("Args: $map")
+main(map, 'patches')
